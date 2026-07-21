6ba7d70934b45ce2038fbfc44ea2157c6f94d267
Bongolethu <fumbatha.bongolethu@gmail.com>
2026-07-21 19:06:56 +0200

    update variable.tf

--- src/infra/terraform/variables.tf
+++ src/infra/terraform/variables.tf
@@ -1,11 +1,23 @@
+# variables.tf
+
+variable "gcp_project_id" {
+  type        = string
+  description = "The GCP Project ID where resources will be created"
+}
+
+variable "databricks_host" {
+  type        = string
+  description = "The Databricks workspace URL (e.g., https://<instance-id>.cloud.databricks.com)"
+}
+
 variable "prefix" {
   type        = string
-  description = "Prefix for resources"
+  description = "Prefix applied to created resources"
   default     = "bongo"
 }
 
 variable "gcp_region" {
   type        = string
-  description = "GCP Region"
+  description = "The GCP region for storage buckets and compute resources"
   default     = "us-central1"
 }
