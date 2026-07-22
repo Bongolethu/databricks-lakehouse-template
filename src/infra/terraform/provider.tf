54397f2d43084084aa17f3eebf9c9237c69e57d1
Bongolethu <fumbatha.bongolethu@gmail.com>
2026-07-22 14:58:34 +0200

    Update provider.tf

--- src/infra/terraform/provider.tf
+++ src/infra/terraform/provider.tf
@@ -12,7 +12,6 @@ terraform {
     }
   }
 
-  # Configures GCS Bucket to hold your Terraform state files
   backend "gcs" {
     bucket = "bongo-143414-tfstate"
     prefix = "terraform/state"
@@ -24,23 +23,19 @@ provider "google" {
   region  = var.gcp_region
 }
 
-# 1. Databricks ACCOUNT Provider (Alias: accounts)
-# Authenticates using Databricks Client ID and Client Secret
+# 1. Databricks ACCOUNT Provider
 provider "databricks" {
   alias         = "accounts"
   host          = "https://accounts.gcp.databricks.com"
   account_id    = var.databricks_account_id
   client_id     = var.databricks_client_id
   client_secret = var.databricks_client_secret
-  auth_type     = "oauth-mws" # Forces OAuth and prevents Google provider conflicts
 }
 
-# 2. Databricks WORKSPACE Provider (Alias: workspace)
-# Inherits the newly generated workspace URL and uses the same credentials
+# 2. Databricks WORKSPACE Provider
 provider "databricks" {
   alias         = "workspace"
   host          = databricks_mws_workspaces.this.workspace_url
   client_id     = var.databricks_client_id
   client_secret = var.databricks_client_secret
-  auth_type     = "oauth-mws" # Forces Workspace OAuth
 }
