9259fa47e757812fa00e6f467697b789828168fe
Bongolethu <fumbatha.bongolethu@gmail.com>
2026-07-22 14:14:01 +0200

    start_setup_terraform

--- src/infra/terraform/provider.tf
+++ src/infra/terraform/provider.tf
@@ -1,51 +1,40 @@
-﻿# provider.tf
-terraform {
-  required_version = ">= 1.0"
+﻿terraform {
+  required_version = ">= 1.5.0"
+
   required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "~> 5.0"
-    }
     databricks = {
       source  = "databricks/databricks"
       version = "~> 1.0"
     }
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0"
+    }
   }
 
   backend "gcs" {
-    bucket = "bongo-143414-tfstate"
-    prefix = "terraform/state"
+    # Backend configuration passed via backend config or pipeline
   }
 }
 
+# 1. Primary / Default Databricks Provider (Workspace level)
+provider "databricks" {
+  host          = var.databricks_host
+  client_id     = var.databricks_client_id
+  client_secret = var.databricks_client_secret
+}
+
+# 2. MWS / Account-Level Databricks Provider (Aliased)
+provider "databricks" {
+  alias         = "mws"
+  host          = "https://accounts.gcp.databricks.com"
+  account_id    = var.databricks_account_id
+  client_id     = var.databricks_client_id
+  client_secret = var.databricks_client_secret
+}
+
+# 3. Google Cloud Provider
 provider "google" {
   project = var.gcp_project_id
   region  = var.gcp_region
-}
-
-# 1. Fetch your active Google Cloud OAuth token
-data "google_client_config" "current" {}
-
-# 2  Provider for Account-level resources (mws_workspaces)
-provider "databricks" {
-  alias         = "mws"
-  host          = "https://accounts.gcp.databricks.com" # or accounts.cloud.databricks.com / accounts.azuredatabricks.net
-  account_id    = var.databricks_account_id
-  client_id     = var.databricks_client_id
-  client_secret = var.databricks_client_secret
-}
-# Provider for Account-level resources (mws_workspaces)
-provider "databricks" {
-  alias         = "mws"
-  host          = "https://accounts.gcp.databricks.com" # or accounts.cloud.databricks.com / accounts.azuredatabricks.net
-  account_id    = var.databricks_account_id
-  client_id     = var.databricks_client_id
-  client_secret = var.databricks_client_secret
-}
-
-# 3. Force the Databricks WORKSPACE Provider to use your Google Cloud token
-provider "databricks" {
-  alias = "workspace"
-  host  = databricks_mws_workspaces.this.workspace_url
-  token = data.google_client_config.current.access_token
 }
--- src/infra/terraform/variables.tf
+++ src/infra/terraform/variables.tf
@@ -1,24 +1,32 @@
-# variables.tf
 variable "gcp_project_id" {
   type        = string
-  default     = "bongo-143414"
-  description = "The Google Cloud Project ID"
+  description = "Google Cloud Project ID"
 }
 
 variable "gcp_region" {
   type        = string
+  description = "GCP Region"
   default     = "us-central1"
-  description = "The Google Cloud Region"
 }
 
 variable "databricks_account_id" {
   type        = string
-  description = "Your Databricks Account ID"
+  description = "Databricks Account ID"
+}
+
+variable "databricks_host" {
+  type        = string
+  description = "Databricks Workspace URL"
+}
+
+variable "databricks_client_id" {
+  type        = string
+  description = "Databricks Service Principal Client ID"
   sensitive   = true
 }
 
-variable "workspace_name" {
+variable "databricks_client_secret" {
   type        = string
-  default     = "bongo-db-workspace"
-  description = "The name of the Databricks workspace"
+  description = "Databricks Service Principal Client Secret"
+  sensitive   = true
 }
