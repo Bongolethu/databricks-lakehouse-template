ddabc86128564e3f8f6147d18bdeb357c79efbcc
Bongolethu <fumbatha.bongolethu@gmail.com>
2026-07-22 15:50:55 +0200

    Update provider.tf

--- src/infra/terraform/provider.tf
+++ src/infra/terraform/provider.tf
@@ -29,8 +29,8 @@ provider "databricks" {
   host          = "https://accounts.gcp.databricks.com"
   # PASTE YOUR ACTUAL VALUES HERE:
   account_id    = "0015ba30-7a2a-469c-94cb-02579c15334a"
-  client_id     = "ed1496a8-d9ab-4e2b-b67e-60a3da484d3f"
-  client_secret = "dose73ea3aaf5377ef781014c3576b31e10b"
+  client_id     = "d03b0302-4a41-4be6-80cd-b1528226c26b"
+  client_secret = "doseacbfff4fd46e7766afd21d644ae95b90"
 }
 
 # 2. Databricks WORKSPACE Provider (HARDCODED)
