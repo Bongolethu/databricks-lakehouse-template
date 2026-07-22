d17dc12de7b0a4eb4edd8f581b662cf8a2609279
Bongolethu <fumbatha.bongolethu@gmail.com>
2026-07-22 15:54:12 +0200

    Update provider.tf

--- src/infra/terraform/provider.tf
+++ src/infra/terraform/provider.tf
@@ -2,42 +2,35 @@
 terraform {
   required_version = ">= 1.0"
   required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "~> 5.0"
-    }
-    databricks = {
-      source  = "databricks/databricks"
-      version = ">= 1.0"
-    }
+    google = { source = "hashicorp/google"; version = "~> 5.0" }
+    databricks = { source = "databricks/databricks"; version = ">= 1.0" }
   }
-
   backend "gcs" {
     bucket = "bongo-143414-tfstate"
     prefix = "terraform/state"
   }
 }
 
+# This fetches the credential of the person running the command
+data "google_client_config" "current" {}
+
 provider "google" {
   project = "bongo-143414"
   region  = "us-central1"
 }
 
-# 1. Databricks ACCOUNT Provider (HARDCODED)
+# 1. ACCOUNT Provider (Using your personal Google Login)
 provider "databricks" {
-  alias         = "accounts"
-  host          = "https://accounts.gcp.databricks.com"
-  # PASTE YOUR ACTUAL VALUES HERE:
-  account_id    = "0015ba30-7a2a-469c-94cb-02579c15334a"
-  client_id     = "d03b0302-4a41-4be6-80cd-b1528226c26b"
-  client_secret = "doseacbfff4fd46e7766afd21d644ae95b90"
+  alias      = "accounts"
+  host       = "https://accounts.gcp.databricks.com"
+  account_id = "PASTE_YOUR_DATABRICKS_ACCOUNT_ID_HERE"
+  # Use your personal Google login token
+  token      = data.google_client_config.current.access_token
 }
 
-# 2. Databricks WORKSPACE Provider (HARDCODED)
+# 2. WORKSPACE Provider
 provider "databricks" {
-  alias         = "workspace"
-  host          = databricks_mws_workspaces.this.workspace_url
-  # PASTE YOUR ACTUAL VALUES HERE: 
-  client_id     = "ed1496a8-d9ab-4e2b-b67e-60a3da484d3f"
-  client_secret = "dose73ea3aaf5377ef781014c3576b31e10b"
+  alias = "workspace"
+  host  = databricks_mws_workspaces.this.workspace_url
+  token = data.google_client_config.current.access_token
 }
