649f5805ce5e9d9791e8f9d4310c9b3caaa3e191
Bongolethu <fumbatha.bongolethu@gmail.com>
2026-07-22 17:24:43 +0200

    Update provider.tf

--- src/infra/terraform/provider.tf
+++ src/infra/terraform/provider.tf
@@ -1,9 +1,14 @@
-﻿# provider.tf
-terraform {
+﻿terraform {
   required_version = ">= 1.0"
   required_providers {
-    google = { source = "hashicorp/google"; version = "~> 5.0" }
-    databricks = { source = "databricks/databricks"; version = ">= 1.0" }
+    google = { 
+      source  = "hashicorp/google"
+      version = "~> 5.0" 
+    }
+    databricks = { 
+      source  = "databricks/databricks"
+      version = ">= 1.0" 
+    }
   }
   backend "gcs" {
     bucket = "bongo-143414-tfstate"
@@ -11,7 +16,6 @@ terraform {
   }
 }
 
-# This fetches the credential of the person running the command
 data "google_client_config" "current" {}
 
 provider "google" {
@@ -19,16 +23,13 @@ provider "google" {
   region  = "us-central1"
 }
 
-# 1. ACCOUNT Provider (Using your personal Google Login)
 provider "databricks" {
   alias      = "accounts"
   host       = "https://accounts.gcp.databricks.com"
-  account_id = "PASTE_YOUR_DATABRICKS_ACCOUNT_ID_HERE"
-  # Use your personal Google login token
+  account_id = "YOUR_DATABRICKS_ACCOUNT_ID"
   token      = data.google_client_config.current.access_token
 }
 
-# 2. WORKSPACE Provider
 provider "databricks" {
   alias = "workspace"
   host  = databricks_mws_workspaces.this.workspace_url
