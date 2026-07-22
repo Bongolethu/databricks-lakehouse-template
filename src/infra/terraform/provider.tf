90348852256b21a950e23599c97dd5d70fed0756
Bongolethu <fumbatha.bongolethu@gmail.com>
2026-07-22 15:14:06 +0200

    Update provider.tf

--- src/infra/terraform/provider.tf
+++ src/infra/terraform/provider.tf
@@ -1,41 +1,12 @@
 ﻿# provider.tf
-terraform {
-  required_version = ">= 1.0"
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "~> 5.0"
-    }
-    databricks = {
-      source  = "databricks/databricks"
-      version = "~> 1.0"
-    }
-  }
+# ... (terraform block remains the same)
 
-  backend "gcs" {
-    bucket = "bongo-143414-tfstate"
-    prefix = "terraform/state"
-  }
-}
-
-provider "google" {
-  project = var.gcp_project_id
-  region  = var.gcp_region
-}
-
-# 1. Databricks ACCOUNT Provider
 provider "databricks" {
   alias         = "accounts"
   host          = "https://accounts.gcp.databricks.com"
   account_id    = var.databricks_account_id
   client_id     = var.databricks_client_id
   client_secret = var.databricks_client_secret
-}
-
-# 2. Databricks WORKSPACE Provider
-provider "databricks" {
-  alias         = "workspace"
-  host          = databricks_mws_workspaces.this.workspace_url
-  client_id     = var.databricks_client_id
-  client_secret = var.databricks_client_secret
+  # ADD THIS LINE:
+  username      = "github-deployer@bongo-143414.iam.gserviceaccount.com"
 }
